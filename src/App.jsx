8d99f2e16b793386167aa3f026abd783423e48e4
AKASH04400 <akash31825@gmail.com>
2025-09-06 08:35:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Car Rental App</h1>
+      <p>Your one-stop solution for renting cars effortlessly.</p>
+    </div>
+  )
+}
+
+export default App
+
